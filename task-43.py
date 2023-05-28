a5eaeabb4c3d34499983895c23467786bac57b34
Victor Firsov <victorf@wallarm.com>
2023-05-28 23:53:47 +0300

    Homework 27.05

---
+++ task-43.py
@@ -0,0 +1,16 @@
+try:
+    num_1 = int(input('Введите число 1 '))
+    num_2 = int(input('Введите число 2 '))
+
+    result = num_1 / num_2
+
+    print(result)
+
+except ValueError:
+    print('Error')
+    quit()
+
+
+
+
+
