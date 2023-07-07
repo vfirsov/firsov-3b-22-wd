056d0d91fa3beadb759e0788dff69be40ab7db4d
Victor Firsov <vfirsov-22-01@edu.ranepa.ru>
2023-07-07 10:06:49 +0300

    DZ 01.07

--- Task-66.py
+++ Task-66.py
@@ -1,24 +1,1 @@
-# Создайте базу данных для хранения информации о студентах, включающую их имена, возвраст, средний балл.
-# Затем выведите все данные на экран
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXIST students
-                        (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, avg_score REAL)''')
-
-
-def insert_students():
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Маслов Артём Александрович", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Смирнов Артемий Лукич", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Верещагина Варвара Ивановна", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Устинов Михаил Артёмович", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Алексеева Валерия Леоновна", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Зорин Фёдор Артёмович", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Иванов Георгий Матвеевич", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Сорокина Есения Павловна", 20, 5))
-    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Демьянов Кирилл Ярославович", 20, 5))
-
-
-# Столкнулся с проблемой повторного добавления данных в таблицу
+# -*- coding: utf-8 -*-import sqlite3conn = sqlite3.connect('database.db')cursor = conn.cursor()cursor.execute('''CREATE TABLE IF NOT EXISTS students                        (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, avg_score REAL)''')def show_students():    cursor.execute("SELECT * FROM students")    rows = cursor.fetchall()    for row in rows:        print(row)def insert_students():    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Maslov Artem Aleksandrovich", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Smirnov Artem Lukich", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Vereshhagina Varvara Ivanovna", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Ustinov Mixail Artyomovich", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Alekseeva Valeriya Leonovna", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Zorin Fyodor Artyomovich", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Ivanov Georgij Matveevich", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Sorokina Eseniya Pavlovna", 20, 5))    cursor.execute("INSERT INTO students (name, age, avg_score) VALUES (?, ?, ?)", ("Dem`yanov Kirill Yaroslavovich", 20, 5))insert_students()show_students()
